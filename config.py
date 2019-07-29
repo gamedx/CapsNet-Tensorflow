6d3d0698cf7376247de5f9a7bb19cc5b41ce00e6
Michael Cruz <mecruz@bu.edu>
2019-07-29 01:02:06 -0400

    tiny test run

--- config.py
+++ config.py
@@ -20,8 +20,8 @@ flags.DEFINE_float('m_minus', 0.1, 'the parameter of m minus')
 flags.DEFINE_float('lambda_val', 0.5, 'down weight of the loss for absent digit classes')
 
 # for training
-flags.DEFINE_integer('batch_size', 24, 'batch size')
-flags.DEFINE_integer('epoch', 30, 'epoch')
+flags.DEFINE_integer('batch_size', 1, 'batch size')
+flags.DEFINE_integer('epoch', 1, 'epoch')
 flags.DEFINE_integer('iter_routing', 2, 'number of iterations in routing algorithm')
 flags.DEFINE_boolean('mask_with_y', False, 'use the true label to mask out target capsule or not') 
 flags.DEFINE_float('stddev', 0.01, 'stddev for W initializer')
@@ -37,7 +37,7 @@ flags.DEFINE_integer('num_threads', 4, 'number of threads of enqueueing examples
 flags.DEFINE_string('logdir', 'logdir', 'logs directory')
 flags.DEFINE_integer('train_sum_freq', 200, 'the frequency of saving train summary(step)')
 flags.DEFINE_integer('val_sum_freq', 500, 'the frequency of saving valuation summary(step)')
-flags.DEFINE_integer('save_freq', 3, 'the frequency of saving model(epoch)')
+flags.DEFINE_integer('save_freq', 1, 'the frequency of saving model(epoch)')
 flags.DEFINE_string('results', 'results', 'path for saving results')
 
 ############################

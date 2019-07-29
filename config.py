0a0c176d91fae8f1ab85a0060a663cf742aa04fe
Michael Cruz <mecruz@bu.edu>
2019-07-29 00:47:17 -0400

    batch size 32

--- config.py
+++ config.py
@@ -20,7 +20,7 @@ flags.DEFINE_float('m_minus', 0.1, 'the parameter of m minus')
 flags.DEFINE_float('lambda_val', 0.5, 'down weight of the loss for absent digit classes')
 
 # for training
-flags.DEFINE_integer('batch_size', 64, 'batch size')
+flags.DEFINE_integer('batch_size', 32, 'batch size')
 flags.DEFINE_integer('epoch', 30, 'epoch')
 flags.DEFINE_integer('iter_routing', 2, 'number of iterations in routing algorithm')
 flags.DEFINE_boolean('mask_with_y', False, 'use the true label to mask out target capsule or not') 
